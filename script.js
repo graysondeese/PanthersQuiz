c1153c72423215d17afc72a5c3afa8b8c995ed2b
Grayson Deese <gldeese@ncsu.edu>
2020-05-06 17:37:00 -0400

    Have the welcome page clearing when button is pressed.

--- script.js
+++ script.js
@@ -1,54 +1,83 @@
-// creating all the variables
-var start = document.querySelector(".start");
-var timeEl = document.querySelector(".time");
-var questionEl = document.createElement("h1");
-var answerEl = document.createElement("p");
+// Creating variables from HTML
+var start = document.querySelector(".start"); // start button
+var timeEl = document.querySelector(".time"); // timer
+var welcome = document.querySelector(".home") // first page shown
+var quizSpace = document.querySelector(".quizArea"); // area where quiz takes place
+var endGame = document.querySelector(".endOfGame"); // page shown @ end of game
+var highScoreLink = document.querySelector(".highScoreLink"); // View highscores link in header
+var highScoreSpace = document.getElementById("highScoreSpace"); // highscore @ end of game
+var questionArea = document.querySelector(".quiz-questions"); // where quiz questions will be shown
+var answerArea = document.querySelector(".answers")
 
-// creating a button
-start.addEventListener("click", function(setTime) {
-// creating a countdown that starts when button is clicked
-    var secondsLeft = 100;
-    function setTime() {
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timeEl.textContent = "Time: " + secondsLeft;
-      
-          if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-          }
-      
-        }, 1000);
-      }
-      setTime();
 
-// making it so questions pop up when the button is clicked
+//Game Variables
+var score = 0;
+var secondsLeft = 0;
+var questions = [];
+var randomNum = 0;
+var highScores;
 
-})
+// Pulling highscores stored in local storage
+if(localStorage.getItem("highScores") === null) {
+  highScores = [];
+} else {
+  highScores = JSON.parse(localStorage.getItem("highScores"));
+}
 
-var count = 0;
+// setting up what happens when game starts
+start.addEventListener("click", startGame);
+
+function startGame() {
+secondsLeft = 5;
+score = 0;
+endGame.innerHTML = "";
+welcome.style.display = "none";
+quizSpace.style.display = "flex";
+arrayReset();
+writeQuestion();
+}
+
 //The array of questions
-var questions = [
+var questionsQuiz = [
   {
-    q: "Who was the first MVP in franchise history?",
-    a:  "Cam Newton"
+    question: "Who was the first MVP in franchise history?",
+    answers: ["Steve Smith", "Christian McCaffrey", "Cam Newton", "Charles Johnson"],
+    correct: "Cam Newton"
   },
   {
     q: "Who did the Panthers draft ninth overall in 2012?",
-    a: "Luke Kuechly, Greg Olsen, Steve Smith, Julius Peppers",
-    correct: "Luke Kuechly",
+    answers: ["Luke Kuechly", "Greg Olsen", "Steve Smith", "Julius Peppers"],
+    correct: "Luke Kuechly"
   },
-]
+];
 
-//Looping every question in the object
-for (let i = 0; i < array.length; index++) {
-
-  var answer = confirm(questions[i].q);
-  
+// function to reset the array so questions get removed as game progresses
+function arrayReset() {
+  for(var i = 0; i < questionsQuiz.length; i ++) {
+    questions[i] = questionsQuiz[i];
+  }
 }
 
-// function nextQuestion() {
-//   if(count === 0) {
+// creating a button event listener function
+start.addEventListener("click", function (setTime) {
+  // creating a countdown that starts when button is clicked
+  function setTime() {
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft;
 
-//   }
-// }
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+        endOfGame();
+      }
+    }, 1000);
+  }
+  setTime();
+});
+
+
+// what shows at the end of the game
+function endOfGame () {
+
+}
+
