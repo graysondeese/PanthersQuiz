60f22c50d2074e06f3f59df3ef811f33c7c5c3fa
Grayson Deese <gldeese@ncsu.edu>
2020-05-06 10:11:25 -0400

    Cleaning up some divs.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // creating all the variables
 var start = document.querySelector(".start");
 var timeEl = document.querySelector(".time");
+var questionEl = document.createElement("h1");
+var answerEl = document.createElement("p");
 
 // creating a button
 start.addEventListener("click", function(setTime) {
@@ -22,3 +24,30 @@ start.addEventListener("click", function(setTime) {
 // making it so questions pop up when the button is clicked
 
 })
+
+var count = 0;
+//The array of questions
+var questions = [
+  {
+    q: "Who was the first MVP in franchise history?",
+    a:  "Cam Newton"
+  },
+  {
+    q: "Who did the Panthers draft ninth overall in 2012?",
+    a: "Luke Kuechly, Greg Olsen, Steve Smith, Julius Peppers",
+    correct: "Luke Kuechly",
+  },
+]
+
+//Looping every question in the object
+for (let i = 0; i < array.length; index++) {
+
+  var answer = confirm(questions[i].q);
+  
+}
+
+// function nextQuestion() {
+//   if(count === 0) {
+
+//   }
+// }
